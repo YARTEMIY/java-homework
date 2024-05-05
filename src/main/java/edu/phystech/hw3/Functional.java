140cdbf37c6ced9efd3855a6e53a1dd20a9833a3
YARTEMIY <yarovikov2003@gmail.com>
2024-05-05 20:47:18 +0500

    do third homework

---
+++ src/main/java/edu/phystech/hw3/Functional.java
@@ -0,0 +1,28 @@
+package edu.phystech.hw3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class Functional {
+
+    public static <T, R> List<R> map(List<T> collection, Function<? super T, R> function) {
+        List<R> result = new ArrayList<>();
+        for (T item : collection) {
+            result.add(function.apply(item));
+        }
+        return result;
+    }
+
+    public static <T> T reduce(List<T> collection, BinaryOperator<T> operator, T identity) {
+        T result = identity;
+        for (T item : collection) {
+            result = operator.apply(result, item);
+        }
+        return result;
+    }
+}
+
+
+
