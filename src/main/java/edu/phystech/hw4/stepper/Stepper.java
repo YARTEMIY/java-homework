57b4d4873634f81a64d7735685736935a2466e70
YARTEMIY <yarovikov2003@gmail.com>
2024-10-12 21:39:33 +0500

    hw5

--- src/main/java/edu/phystech/hw4/stepper/Stepper.java
+++ src/main/java/edu/phystech/hw4/stepper/Stepper.java
@@ -2,6 +2,9 @@ package edu.phystech.hw4.stepper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @author kzlv4natoly
@@ -14,16 +17,49 @@ public class Stepper {
     }
 
     private final List<Side> history = new ArrayList<>();
-    private final Object lock = new Object();
+    private final Lock lock = new ReentrantLock();
+    private final Condition canStepLeft = lock.newCondition();
+    private final Condition canStepRight = lock.newCondition();
     private boolean isLeftTurn = true;
 
     public void leftStep() {
+        lock.lock();
+        try {
+            while (!isLeftTurn) {
+                canStepLeft.await();
+            }
+            history.add(Side.LEFT);
+            isLeftTurn = false;
+            canStepRight.signalAll();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public void rightStep() {
+        lock.lock();
+        try {
+            while (isLeftTurn) {
+                canStepRight.await();
+            }
+            history.add(Side.RIGHT);
+            isLeftTurn = true;
+            canStepLeft.signalAll();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            lock.unlock();
+        }
     }
 
     public List<Side> getHistory() {
-        return history;
+        lock.lock();
+        try {
+            return new ArrayList<>(history);
+        } finally {
+            lock.unlock();
+        }
     }
 }
